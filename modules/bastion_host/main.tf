ac4244ec962705cfede7fb18e21621ab7601b02d
Erich Schudt <erich_schudt@live.com>
2018-08-08 10:48:10 +0100

    Changed name of bastion host server

--- modules/bastion_host/main.tf
+++ modules/bastion_host/main.tf
@@ -2,9 +2,9 @@
 # Create the bastion_host with floating ip to receive all requests
 
 # Create a new Web Droplet in the lon1 region
-resource "digitalocean_droplet" "bastion_host" {
+resource "digitalocean_droplet" "bastion-host" {
   image  = "ubuntu-16-04-x64"
-  name   = "bastion_host"
+  name   = "bastion-host"
   region = "lon1"
   size   = "512mb"
   private_networking = false
@@ -12,10 +12,10 @@ resource "digitalocean_droplet" "bastion_host" {
 }
 
 # Create a static ip to access from public
-resource "digitalocean_floating_ip" "bastion_host" {
-  droplet_id = "${digitalocean_droplet.bastion_host.id}"
-  region     = "${digitalocean_droplet.bastion_host.region}"
+resource "digitalocean_floating_ip" "bastion-host" {
+  droplet_id = "${digitalocean_droplet.bastion-host.id}"
+  region     = "${digitalocean_droplet.bastion-host.region}"
 }
 output "bastion_host_ip" {
-  value = "${digitalocean_floating_ip.bastion_host.ip_address}"
+  value = "${digitalocean_floating_ip.bastion-host.ip_address}"
 }

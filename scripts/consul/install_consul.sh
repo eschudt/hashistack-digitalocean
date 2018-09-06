201159be2fc058def0008dfbcd6e5f733ce8e57d
Erich Schudt <erich_schudt@live.com>
2018-09-06 22:27:56 +0100

    Changed scripts to start process in background

--- scripts/consul/install_consul.sh
+++ scripts/consul/install_consul.sh
@@ -8,11 +8,11 @@ cp consul /usr/bin/
 mkdir /etc/consul.d
 mkdir /tmp/consul
 if [ $1 == "server" ]; then
-	consul agent -server -bootstrap-expect=1 -data-dir=/tmp/consul -node=agent-one -bind=$2 -enable-script-checks=true -config-dir=/etc/consul.d &
+  consul agent -server -bootstrap-expect=1 -data-dir=/tmp/consul -node=agent-one -bind=$2 -enable-script-checks=true -config-dir=/etc/consul.d > consul-server.log 2>&1 &
 else
-  consul agent -data-dir=/tmp/consul -node=agent-two -bind=$2 -enable-script-checks=true -config-dir=/etc/consul.d &
+  consul agent -data-dir=/tmp/consul -node=agent-two -bind=$2 -enable-script-checks=true -config-dir=/etc/consul.d > consul-client.log 2>&1 &
   sleep 5
-	consul join $3
+  consul join $3
 fi
 echo "Installation of Consul complete\n"
 exit 0
--- scripts/nomad/install_nomad.sh
+++ scripts/nomad/install_nomad.sh
@@ -6,11 +6,11 @@ wget https://releases.hashicorp.com/nomad/0.8.4/nomad_0.8.4_linux_amd64.zip
 unzip nomad_0.8.4_linux_amd64.zip
 cp nomad /usr/bin/
 if [ $1 == "server" ]; then
-	nomad agent -config server.hcl &
+	nomad agent -config server.hcl nomad-server.log 2>&1 &
   #sleep 5
   #nomad agent -config client1.hcl &
 else
-	nomad agent -config client2.hcl &
+	nomad agent -config client2.hcl nomad-client.log 2>&1 &
 fi
 echo "Installation of Nomad complete\n"
 exit 0

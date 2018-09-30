f540a3aefa432590d62f5becda6b599713092db9
Erich Schudt <erich_schudt@live.com>
2018-09-30 20:14:40 +0100

    Added export in setup script

--- scripts/vault/setup_vault.sh
+++ scripts/vault/setup_vault.sh
@@ -2,6 +2,7 @@
 
 echo "Setting Up Vault on Server\n"
 # init vautl, unseal vault and export root token
+export VAULT_ADDR=http://127.0.0.1:8200
 
 if [ $1 == "0" ]; then
 	vault operator init > startupOutput.txt

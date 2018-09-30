9b6e050bdfca7ca0d693c4e40eae099284455d0d
Erich Schudt <erich_schudt@live.com>
2018-09-30 20:05:43 +0100

    Added vault address export

--- scripts/vault/install_vault.sh
+++ scripts/vault/install_vault.sh
@@ -11,5 +11,7 @@ cp vault /usr/bin/
 systemctl enable vault-server.service
 systemctl start vault-server.service
 
+export VAULT_ADDR=http://127.0.0.1:8200
+
 echo "Installation of Vault complete\n"
 exit 0
